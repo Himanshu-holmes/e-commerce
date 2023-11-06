54b5e71b6fa4f0ef78bad66634953cab6b2e32f0
Himanshu-holmes <himanshuholmes@gmail.com>
2023-11-06 15:52:37 +0530

    api route test working

--- api/index.js
+++ api/index.js
@@ -1,7 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser';
+import testRouter from './route/user.route';
 
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  app.use(express.json());
+
+  app.use(cookieParser());
+
+  app.use('/api', testRouter)
+  app.get('/', (req, res) => {
+    res.send('Sambhu Bhai');
+  })
+  
+
 app.listen(3000, () => {
-    console.log('Server listening on port 3000!!');
+    console.log(`Example app listening at http://localhost:3000`);
 });
