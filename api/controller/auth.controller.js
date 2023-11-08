05cd0c2d2cd5560c90b64d60295736472e22fdca
Himanshu-holmes <himanshuholmes@gmail.com>
2023-11-08 15:58:18 +0530

    created signin route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,12 +1,57 @@
 import User from "../model/user.model.js"
+
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs'
 ;    
-export const signup = async(req,res,next) => {
+export const signup = async(req,res,next)=>{
+    const {username, email, password} = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({username, email, password: hashedPassword});
+
     try {
-        const {username, email, password} = req.body;
-        const user = await User.create({username, email, password});
+
+        await newUser.save()
+        res.status(201).json({
+              message:"User created successfully",
+        });
         
-        res.status(201).json(user);
+        
     } catch (error) {
+       next(error)
+    }
+
+
+}
+
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+
+
+    try {
+        const validUser = await User.findOne({email});
+        
+        if(!validUser) return next(errorHandler(404, "User not found"));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        console.log(validPassword);
+
+        if(!validPassword) return next(errorHandler(401, "Wrong credential!"));
+       
+       
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+        
+       
+        const {password:Pass, ...rest} = validUser._doc;
+        res.cookie('access_token',token, {httpOnly:true
+        })
+        .status(200)
+        .json(rest)
+
+        
+    } catch (error) {
+      
         next(error)
     }
 }
--- api/index.js
+++ api/index.js
@@ -3,10 +3,14 @@ import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser';
 import testRouter from './route/user.route.js';
 import authRouter from './route/auth.route.js'
+import { test } from './controller/user.controller.js';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+console.log(typeof(process.env.MONGO))
 
 mongoose
   .connect(process.env.MONGO)
@@ -23,9 +27,10 @@ mongoose
 
 
   app.use('/api/auth', authRouter)
-  app.get('/', (req, res) => {
-    res.send('Sambhu Bhai');
+  app.get('/',(req,res) => {
+    res.send('Hello World!')
   })
+
   
 
 app.listen(3000, () => {
